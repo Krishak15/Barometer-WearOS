9f294b5eb7cd1d799b885538f4346cef9a3dd9b8
Krishak15 <anandevu2@gmail.com>
2023-09-25 10:30:53 +0530

    platform specific code for rotary

--- android/app/src/main/kotlin/com/example/barometer_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/barometer_app/MainActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import androidx.annotation.NonNull
 import android.view.MotionEvent
 
+import com.samsung.wearable_rotary.WearableRotaryPlugin
+
 class MainActivity: FlutterActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
         intent.putExtra("background_mode", "transparent")
         super.onCreate(savedInstanceState)
+    }
+     override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
+        return when {
+            WearableRotaryPlugin.onGenericMotionEvent(event) -> true
+            else -> super.onGenericMotionEvent(event)
+        }
     }
 }
