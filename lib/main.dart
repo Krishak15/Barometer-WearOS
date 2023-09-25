318eefd236ee214736afe1125cccbaab88b4eca6
Krishak15 <anandevu2@gmail.com>
2023-09-25 18:31:11 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ void main() async {
         ChangeNotifierProvider(create: (_) => ReverseGeoCodingProvider()),
         // ChangeNotifierProvider(create: (_) => LocationProvider()),
       ],
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
@@ -50,7 +50,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: MainScreen()
+        home: const MainScreen()
 
         // MyPageView(
         //   pageController: _pageController,
