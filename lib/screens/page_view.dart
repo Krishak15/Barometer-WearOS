91633666f8762fb75947efa23790dc0005abe8c1
Krishak15 <anandevu2@gmail.com>
2023-09-25 10:33:53 +0530

    -

--- lib/screens/page_view.dart
+++ lib/screens/page_view.dart
@@ -1,32 +1,57 @@
-import 'package:barometer_app/screens/graph_screen.dart';
-import 'package:barometer_app/screens/main_screen.dart';
-import 'package:flutter/material.dart';
+// import 'package:barometer_app/screens/details_screen.dart';
 
-class MyPageView extends StatefulWidget {
-  const MyPageView({super.key});
+// import 'package:barometer_app/screens/main_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:rotary_scrollbar/rotary_scrollbar.dart';
 
-  @override
-  MyPageViewState createState() => MyPageViewState();
-}
+// class MyPageView extends StatefulWidget {
+//   const MyPageView({super.key});
 
-class MyPageViewState extends State<MyPageView> {
-  final PageController _pageController = PageController();
-  int _currentPageIndex = 0;
+//   @override
+//   MyPageViewState createState() => MyPageViewState();
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return PageView(
-      controller: _pageController,
-      onPageChanged: (index) {
-        setState(() {
-          _currentPageIndex = index;
-        });
-      },
-      scrollDirection: Axis.vertical,
-      children: const [
-        MainScreen(),
-        GraphScreen(),
-      ],
-    );
-  }
-}
+// final PageStorageBucket bucket = PageStorageBucket();
+
+// class MyPageViewState extends State<MyPageView> {
+//   final pageController = PageController();
+ 
+
+//     @override
+//   void initState() {
+//     super.initState();
+//     pageController.addListener(() {
+//       setState(() {
+//         _currentPageIndex = pageController.page?.round() ?? 0;
+//       });
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     pageController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: RotaryScrollWrapper(
+//         rotaryScrollbar: RotaryScrollbar(controller: pageController),
+//         child: PageView(
+//           controller: pageController,
+//           // onPageChanged: (index) {
+//           //   setState(() {
+//           //     _currentPageIndex = index;
+//           //   });
+//           // },
+//           scrollDirection: Axis.vertical,
+//           children: const [
+//             MainScreen(),
+//             DetailsScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import firebase_core
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
