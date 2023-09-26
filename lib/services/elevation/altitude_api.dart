93cc24562774c0e976d420733b9e87b719513dee
Krishak15 <anandevu2@gmail.com>
2023-09-26 15:18:29 +0530

    Changed API client to api.open-elevation.com

--- lib/services/elevation/altitude_api.dart
+++ lib/services/elevation/altitude_api.dart
@@ -4,46 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-// class AltitudeApiProvider with ChangeNotifier {
-//   Map<String, dynamic> altitude = {};
-//   double altitudeString = 0;
-//   double get elevation => altitudeString;
-//   bool isLoading = true;
-
-//   Future<void> fetchAltitudeData(BuildContext context) async {
-//     isLoading = true;
-
-//     Provider.of<FirebaseDataProvider>(context, listen: false).fetchData();
-
-//     final firebaseDataProvider = Provider.of<FirebaseDataProvider>(context);
-//     firebaseDataProvider.fetchData();
-//     String lat = firebaseDataProvider.latitude.toStringAsFixed(2);
-//     String long = firebaseDataProvider.longitude.toStringAsFixed(2);
-
-//     print("---------------------${firebaseDataProvider.latitude}");
-
-//     LocationProvider coordinates = LocationProvider();
-//     await coordinates.getCurrentLocation();
-
-//     final coordinatesval =
-//         'latitude=${coordinates.latitude!.toStringAsFixed(2)}&longitude=${coordinates.longitude!.toStringAsFixed(2)}';
-//     // print(coordinatesval);
-//     final url =
-//         Uri.parse("https://api.open-meteo.com/v1/elevation?$coordinatesval");
-
-//     final response = await http.get(url);
-
-//     if (response.statusCode == 200) {
-//       final altitudeData = jsonDecode(response.body);
-
-//       altitudeString = altitudeData['elevation'][0];
-//       isLoading = false;
-
-//       notifyListeners();
-//     }
-//   }
-// }
-
 class AltitudeApiProvider with ChangeNotifier {
   Map<String, dynamic> altitude = {};
   double altitudeString = 0;
@@ -52,9 +12,9 @@ class AltitudeApiProvider with ChangeNotifier {
 
   Future<void> fetchAltitudeData(
       BuildContext context, latitude, longitude) async {
-    final coordinatesval = 'latitude=$latitude&longitude=$longitude';
-    final url =
-        Uri.parse("https://api.open-meteo.com/v1/elevation?$coordinatesval");
+    final coordinatesval = '$latitude,$longitude';
+    final url = Uri.parse(
+        "https://api.open-elevation.com/api/v1/lookup?locations=$coordinatesval");
 
     final response = await http.get(url);
 
@@ -64,9 +24,15 @@ class AltitudeApiProvider with ChangeNotifier {
       }
       isLoading = true;
       notifyListeners();
+      if (kDebugMode) {
+        print(response.body);
+      }
       final altitudeData = jsonDecode(response.body);
 
-      altitudeString = altitudeData['elevation'][0];
+      altitudeString = altitudeData['results'][0]['elevation'];
+      if (kDebugMode) {
+        print(altitudeString);
+      }
 
       isLoading = false;
 
