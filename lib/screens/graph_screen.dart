f0a649ce7c6d3687e9138fb8d0a43a7ffff455b2
Krishak15 <anandevu2@gmail.com>
2023-09-26 15:16:19 +0530

    -

--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -1,23 +1,23 @@
-import 'package:barometer_app/chart/atm_chart.dart';
-import 'package:flutter/material.dart';
+// import 'package:barometer_app/chart/atm_chart.dart';
+// import 'package:flutter/material.dart';
 
-class GraphScreen extends StatefulWidget {
-  const GraphScreen({super.key});
+// class GraphScreen extends StatefulWidget {
+//   const GraphScreen({super.key});
 
-  @override
-  State<GraphScreen> createState() => _GraphScreenState();
-}
+//   @override
+//   State<GraphScreen> createState() => _GraphScreenState();
+// }
 
-class _GraphScreenState extends State<GraphScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-          child: const Center(child: LineChartSample2())),
-    );
-  }
-}
+// class _GraphScreenState extends State<GraphScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Container(
+//           height: double.infinity,
+//           width: double.infinity,
+//           color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+//           child: const Center(child: LineChartSample2())),
+//     );
+//   }
+// }
